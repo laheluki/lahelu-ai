d6ed8c18e72499ec75dccac8ed1b94c8558cfa81
laheluki <rifkisaputra225@gmail.com>
2024-09-17 11:58:38 +0700

    setup font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import { Space_Grotesk } from 'next/font/google';
+
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+const font = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
@@ -26,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${font.className} antialiased`}>
         <ThemeProvider
           attribute='class'
           defaultTheme='dark'
